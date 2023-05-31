4a7416684d987020c883509a8552d0a84fefbebe
Luciana Garro <garrocpp@gmail.com>
2023-05-30 21:13:09 -0300

    finalizamos chat

--- src/app.js
+++ src/app.js
@@ -17,5 +17,23 @@ app.use('/', viewsRouter)
 
 const server = app.listen(8081, () => console.log('Server running'));
 const io = new Server (server);
+const messages = [];
+
+io.on ('connection', socket =>{
+    console.log('Nuevo cliente conectado');
+
+
+socket.on('message', data => {
+    messages.push(data);
+    io.emit('messageLogs', messages)
+})
+
+socket.on ('authenticated', data =>{
+    socket.emit ('messageLogs', messages);
+    socket.broadcast.emit('newUserConnected', data);
+});
+})
+
+
 
 
